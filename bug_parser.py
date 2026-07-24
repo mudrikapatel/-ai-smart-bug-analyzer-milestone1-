a5af1e284dc6e1dc5e045a8eeb5b93dc3530cca7
Mudrika Patel <mudrikapatel04@gmail.com>
2026-07-24 17:15:12 +0530

    Add files via upload

--- bug_parser.py
+++ bug_parser.py
@@ -1,26 +1,13 @@
 def parse_file(filepath):
-    """
-    Read TXT, LOG or PDF file and return its text.
-    """
 
     if filepath.endswith(".txt") or filepath.endswith(".log"):
+
         with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
-            return f.read()
 
-    elif filepath.endswith(".pdf"):
-        import PyPDF2
+            text = f.read()
 
-        text = ""
+            print("========== BUG TEXT ==========")
+            print(text[:1000])
+            print("==============================")
 
-        with open(filepath, "rb") as f:
-            reader = PyPDF2.PdfReader(f)
-
-            for page in reader.pages:
-                extracted = page.extract_text()
-                if extracted:
-                    text += extracted
-
-        return text
-
-    else:
-        return ""
+            return text
